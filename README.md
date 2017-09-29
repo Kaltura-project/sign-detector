6af4459a601ccf71e7bae99426bbd0a9b1f6d083
Vijay Pillai <vijay.pillai397@gmail.com>
2017-09-29 11:20:38 -0400

    first classifier run, re-add original dataset

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # sign-detector
 Stop Sign Dataset from https://github.com/mbasilyan/Stop-Sign-Detection
+<br>
 HaarTraining dir from https://github.com/sonots/tutorial-haartraining
--- detector.py
+++ detector.py
@@ -1,6 +1,18 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 
 import cv2
 import numpy as np
 
-DATASET = "Stop\ Sign\ Dataset"
+sign_cascade = cv2.CascadeClassifier("classifier/cascade.xml")
+
+img = cv2.imread('test.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+signs = sign_cascade.detectMultiScale(gray, 1.1, 5)
+print(signs)
+for sign in range(np.size(signs, 0)):
+    x_pos, y_pos, width, height = signs[sign]
+    cv2.imwrite(str(sign) + ".png", img[sign[0]:sign[0] + sign[2] + sign[1], sign[1] + sign[3]])
+
+
+cv2.destroyAllWindows()
